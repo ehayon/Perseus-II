832566c2da738bae090067fa2fb7f0ee4b393c7a
Ethan Hayon <ehayon@gmail.com>
2013-03-19 11:26:01 -0400

    added some skeleton header file for inertial measurement module

---
+++ src/modules/imu/imu.h
@@ -0,0 +1,20 @@
+/**
+ * The IMU module is responsible for orientation determination 
+ * and acts as an Attitude Heading Reference System (AHRS)
+ *
+ * We will use quaternion based Kalman filtering to fuse 
+ * Magnetometer, Accelerometer, and Rate Gyroscope (MARG) data into 
+ * a clean (low-bias) attitude determination system.
+ */
+
+#ifndef _IMU_H_
+#define _IMU_H_
+
+float g_x, g_y, g_z;
+float a_x, a_y, a_z;
+float m_x, m_y, m_z;
+
+/* Update the current state using Kalman state estimation filter */
+void update_filter(float, float, float, float, float, float, float, float, float);
+
+#endif
