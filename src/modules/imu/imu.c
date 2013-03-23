8dc111c2f4f3dd2ad9320397d1f8e658e83cb2a6
Ethan Hayon <ehayon@gmail.com>
2013-03-23 10:48:35 -0400

    restructured project and created makefile for modules

---
+++ src/modules/imu/imu.c
@@ -0,0 +1,18 @@
+#include "../lib/imu.h"
+
+extern float g_x;
+extern float g_y;
+extern float g_z;
+extern float a_x;
+extern float a_y;
+extern float a_z;
+extern float m_x;
+extern float m_y;
+extern float m_z;
+
+/**
+ * Update the Kalman filter with new MARG data
+ */
+void update_filter(float g_x, float g_y, float g_z, float a_x, float a_y, float a_z, float m_x, float m_y, float m_z) {
+
+} 
